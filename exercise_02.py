3e19f77f3fa7b6aa2bc800a9b7abeb8ff9943359
GabrielDully <gabrieldully@usp.br>
2024-02-17 18:35:29 -0300

    exercise_2.py
    Addition of the resolution of 'exercise_2.py'

--- exercise_02.py
+++ exercise_02.py
@@ -3,3 +3,23 @@ File Renaming:
 Create a Python script that renames all text files (with the extension .txt) in a given directory by adding a prefix "new_" to their filenames. Use pathlib for directory traversal and file renaming.
 '''
 
+
+from pathlib import Path
+
+def rename_txt_files(directory_str):
+
+    directory_path = Path(directory_str)
+
+    if not directory_path.exists():
+        return False 
+
+    prefix = '/new_'
+    suffix = '.txt'
+    i = 1
+    for item in directory_path.iterdir():
+
+        item_path = Path(item)
+        if item_path.suffix == '.txt':
+            
+            item_path.rename(directory_str + prefix + str(i) + suffix)
+            i += 1
